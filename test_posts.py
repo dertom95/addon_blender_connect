6e4613203bbb9c7e431ea1c9ad537274a720c1dc
Thomas Trocha <thomas.trocha@gmail.com>
2019-07-28 09:06:33 +0200

    helper to test the system

---
+++ test_posts.py
@@ -0,0 +1,17 @@
+from BConnectNetwork import StartNetwork, Publish, AddListener 
+import time
+
+StartNetwork(subPort="5559",pubPort="5560",initialFilter=b"",startThreads=True)
+
+def Incoming(topic,subtype,data):
+    print("Listener(%s): %s" % (topic,data) )
+
+print("waiting..")
+time.sleep(1)
+AddListener("forty",Incoming)
+AddListener("blender",Incoming)
+time.sleep(1)
+
+while True:
+    Publish("blender kick text",b"f95")
+    time.sleep(5)  
